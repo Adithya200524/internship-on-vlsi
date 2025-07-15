50c6a0f8d046e1bb89d62927815cee749c479781
Adithya200524 <jayaadithya86@gmail.com>
2025-07-15 11:16:32 +0530

    Update ram.sv

--- ram.sv
+++ ram.sv
@@ -1,28 +1,20 @@
 module ram(clk,addres,data_in,en,we,data_out);
-
 input clk,en,we;
-
 input[2:0] addres;
-
 input[7:0] data_in;
-
 output reg [7:0] data_out;
 
 reg [7:0] mem [0:7];
 
-initial begin data_out = 8'b00000000; end
-
+    initial begin data_out = 8'b00000000; end
 always@(posedge clk)
-  
-if(en)begin
-
+    if(en)begin
 if(we)
-  
-mem[addres]=data_in;
-else
+    mem[addres]=data_in;
+  else
 data_out=mem[addres];
 end
-else
+  else
 data_out = data_out;
 
 endmodule
